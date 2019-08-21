877204c1f0b9881afd5c6facaf0f511154a279d5
Michael Kleinhenz <kleinhenz@redhat.com>
2019-08-21 13:29:22 +0200

    CICD fixes.

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push kleinhenz/registration-service:0.1
 docker push kleinhenz/registration-service:latest

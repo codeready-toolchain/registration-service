738b82e0c178649fcf3b4514ed6ded44a86a568d
Michael Kleinhenz <kleinhenz@redhat.com>
2019-08-21 15:02:38 +0200

    Fix for port listener.

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func main() {
 	// finally, create and start the service
 	srv := &http.Server{
 		Handler:      router,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "0.0.0.0:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}

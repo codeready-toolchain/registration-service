50e37db9234a7896db5afe6586910baca276e11e
Michael Kleinhenz <kleinhenz@redhat.com>
2019-08-30 20:45:33 +0200

    Fixed coverage.

--- make/clean.mk
+++ make/clean.mk
@@ -3,4 +3,5 @@
 clean:
 	@cd "$(GOPATH)/src/github.com/codeready-toolchain/registration-service" && \
 		rm -f pkg/static/generated_assets.go && \
+		rm -rf $(COV_DIR) && \
 		rm -f registration-service
--- make/test.mk
+++ make/test.mk
@@ -1,4 +1,12 @@
+COV_DIR = coverage
+
 .PHONY: test
 ## runs all tests with bundles assets
 test: generate
-	go test -count=1 ./...
+	@-mkdir -p $(COV_DIR)
+	@-rm -f $(COV_DIR)/coverage.txt
+	go test -count=1 -coverprofile=$(COV_DIR)/profile.out -covermode=atomic ./...
+ifeq (,$(wildcard $(COV_DIR)/profile.out))
+	cat $(COV_DIR)/profile.out >> $(COV_DIR)/coverage.txt
+	rm $(COV_DIR)/profile.out
+endif 

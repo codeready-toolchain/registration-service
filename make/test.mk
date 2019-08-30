79377e0de6d2d39ff387739739e10559d9b7f9be
Michael Kleinhenz <kleinhenz@redhat.com>
2019-08-30 21:23:06 +0200

    Test /test seperately.

--- make/test.mk
+++ make/test.mk
@@ -5,4 +5,7 @@ COV_DIR = coverage
 test: generate
 	@-mkdir -p $(COV_DIR)
 	@-rm -f $(COV_DIR)/coverage.txt
+	# test the test package seperately, no coverage report
+	go test -count=1 ./test
+	# test the rest of the code, with coverage report
 	go test -count=1 -coverprofile=$(COV_DIR)/coverage.txt -covermode=atomic $(shell go list ./... | grep -v /test)

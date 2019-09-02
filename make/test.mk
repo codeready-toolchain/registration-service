e27ed6ae28734cd9593acaa02eef897fe05f79c1
Michael Kleinhenz <kleinhenz@redhat.com>
2019-09-02 14:35:55 +0200

    Fix coverage reports (#9)
    * Fixed coverage reports.

--- make/test.mk
+++ make/test.mk
@@ -1,4 +1,9 @@
+COV_DIR = coverage
+GOPKGS = $(shell go list ./...)
+
 .PHONY: test
 ## runs all tests with bundles assets
 test: generate
-	go test -count=1 ./...
+	@-mkdir -p $(COV_DIR)
+	@-rm -f $(COV_DIR)/coverage.txt
+	go test -timeout 10m -coverprofile=profile.out -covermode=atomic -coverpkg=$$GOPKGS ./...

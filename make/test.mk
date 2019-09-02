4b40afb16a8c1791d98fee102847b00800478944
Michael Kleinhenz <kleinhenz@redhat.com>
2019-09-02 10:35:03 +0200

    Included test package.

--- make/test.mk
+++ make/test.mk
@@ -1,18 +1,9 @@
 COV_DIR = coverage
-GOPKGS = $(shell go list ./... | grep -v /test)
+GOPKGS = $(shell go list ./...)
 
 .PHONY: test
 ## runs all tests with bundles assets
 test: generate
-	# test the test package seperately, no coverage report
-	go test -count=1 ./test
-	# test the rest of the code, with coverage report	
 	@-mkdir -p $(COV_DIR)
 	@-rm -f $(COV_DIR)/coverage.txt
-	@-for d in $(GOPKGS) ; do \
-	  go test -coverprofile=profile.out -covermode=atomic -coverpkg=$$GOPKGS $$d ; \
-    if [ -f profile.out ]; then \
-      cat profile.out >> $(COV_DIR)/coverage.txt ; \
-      rm profile.out ; \
-    fi ; \
-	done
+	go test -timeout 10m -coverprofile=profile.out -covermode=atomic -coverpkg=$$GOPKGS ./...

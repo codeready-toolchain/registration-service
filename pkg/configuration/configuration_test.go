b4141fd2783a33e966a26e8048115aa28b328965
Michael Kleinhenz <kleinhenz@redhat.com>
2019-09-13 17:19:05 +0200

    Added keys url configuration.

--- pkg/configuration/configuration_test.go
+++ pkg/configuration/configuration_test.go
@@ -296,3 +296,35 @@ func TestIsTestingMode(t *testing.T) {
 		assert.Equal(t, newVal, config.IsTestingMode())
 	})
 }
+
+func TestGetAuthClientPublicKeysURL(t *testing.T) {
+	key := configuration.EnvPrefix + "_" + "AUTH_CLIENT_PUBLIC_KEYS_URL"
+	resetFunc := testutils.UnsetEnvVarAndRestore(key)
+	defer resetFunc()
+
+	t.Run("default", func(t *testing.T) {
+		resetFunc := testutils.UnsetEnvVarAndRestore(key)
+		defer resetFunc()
+		config := getDefaultConfiguration(t)
+		assert.Equal(t, configuration.DefaultAuthClientPublicKeysURL, config.GetAuthClientPublicKeysURL())
+	})
+
+	t.Run("file", func(t *testing.T) {
+		resetFunc := testutils.UnsetEnvVarAndRestore(key)
+		defer resetFunc()
+		u, err := uuid.NewV4()
+		require.NoError(t, err)
+		newVal := u.String()
+		config := getFileConfiguration(t, `auth_client.public_keys_url: "`+newVal+`"`)
+		assert.Equal(t, newVal, config.GetAuthClientPublicKeysURL())
+	})
+
+	t.Run("env overwrite", func(t *testing.T) {
+		u, err := uuid.NewV4()
+		require.NoError(t, err)
+		newVal := u.String()
+		os.Setenv(key, newVal)
+		config := getDefaultConfiguration(t)
+		assert.Equal(t, newVal, config.GetAuthClientPublicKeysURL())
+	})
+}

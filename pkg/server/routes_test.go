09ec1dcf76c340c2c75e33e8898a537254c04aca
Tina Kurian <tkurian@redhat.com>
2019-09-12 20:59:14 -0400

    Rename registrationserver to server (#18)
    * renaming registrationserver to server
    
    * fixing tests

--- pkg/server/routes_test.go
+++ pkg/server/routes_test.go
@@ -1,4 +1,4 @@
-package registrationserver_test
+package server_test
 
 import (
 	"bytes"
@@ -17,7 +17,7 @@ import (
 func TestStaticContent(t *testing.T) {
 
 	// Create handler instance.
-	static := registrationserver.StaticHandler{Assets: static.Assets}
+	static := server.StaticHandler{Assets: static.Assets}
 
 	// Setting up the table test
 	var statictests = []struct {

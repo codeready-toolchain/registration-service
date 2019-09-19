0991ad50b6f7c52a373d2f4b12ed7b1ddafa4dff
Shane Bryzak <sbryzak@gmail.com>
2019-09-20 09:45:52 +1000

    CRT-245 fixed all compile errors, finished refactor
    Signed-off-by: Shane Bryzak <sbryzak@gmail.com>

--- pkg/signup/signup_client.go
+++ pkg/signup/signup_client.go
@@ -1,113 +1,100 @@
 package signup
 
 import (
-	"context"
-	"fmt"
 	crtapi "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
-	//"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
-	apiextensionv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apiextension "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/rest"
-	"strings"
 )
 
-func NewSignupClient() (*signupClientImpl, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
+const (
+	USER_SIGNUP_RESOURCE_NAME = "usersignups"
+)
 
+func NewUserSignupClient(cfg *rest.Config) (*UserSignupV1Alpha1Client, error) {
 	scheme := runtime.NewScheme()
 	crtapi.SchemeBuilder.AddToScheme(scheme)
 	crtapi.SchemeBuilder.Register(&crtapi.UserSignup{}, &crtapi.UserSignupList{})
+
+	config := *cfg
 	config.GroupVersion = &crtapi.SchemeGroupVersion
 	config.APIPath = "/apis"
 	config.ContentType = runtime.ContentTypeJSON
 	config.NegotiatedSerializer = serializer.WithoutConversionCodecFactory{CodecFactory: serializer.NewCodecFactory(scheme)}
 
-	client, err := rest.RESTClientFor(config)
+	client, err := rest.RESTClientFor(&config)
 	if err != nil {
 		return nil, err
 	}
 
-	/*
-		clientset, err := apiextension.NewForConfig(config)
-		if err != nil {
-			return nil, err
-		}*/
-
-	return &signupClientImpl{
-		Clientset: client,
+	return &UserSignupV1Alpha1Client{
+		restClient: client,
 	}, nil
 }
 
-func (c *signupClientImpl) CreateUserSignup(ctx context.Context, username, userID string) (*crtapi.UserSignup, error) {
-	name, err := c.transformAndValidateUserName(username)
-	if err != nil {
-		return nil, err
-	}
-
-	userSignup := &crtapi.UserSignup{
-		ObjectMeta: v1.ObjectMeta{
-			Name: name,
-		},
-		Spec: crtapi.UserSignupSpec{
-			UserID:        userID,
-			TargetCluster: "",
-			Approved:      false,
-			Username:      username,
-		},
-	}
-
-	created, err := c.Clientset.ApiextensionsV1().CustomResourceDefinitions().Create(userSignup.(*apiextensionv1.CustomResourceDefinition))
-	if err != nil {
-		return nil, err
-	}
-
-	return created.(*crtapi.UserSignup), nil
+type UserSignupV1Alpha1Client struct {
+	restClient rest.Interface
 }
 
-func (c *signupClientImpl) transformAndValidateUserName(username string) (string, error) {
-	replaced := strings.ReplaceAll(strings.ReplaceAll(username, "@", "-at-"), ".", "-")
-
-	iteration := 0
-	transformed := replaced
-
-	for {
-		userSignup, err := c.getUserSignup(transformed)
-		if err != nil {
-			if !errors.IsNotFound(err) {
-				return "", err
-			}
-		}
-
-		if userSignup == nil {
-			break
-		}
-
-		iteration++
-		transformed = fmt.Sprintf("%s-%d", replaced, iteration)
+func (c *UserSignupV1Alpha1Client) UserSignups(namespace string) UserSignupClient {
+	return &userSignupClientImpl{
+		client: c.restClient,
+		ns:     namespace,
 	}
-
-	return transformed, nil
 }
 
-func (c *signupClientImpl) getUserSignup(name string) (*crtapi.UserSignup, error) {
+type UserSignupClient interface {
+	Get(name string) (*crtapi.UserSignup, error)
+	Create(obj *crtapi.UserSignup) (*crtapi.UserSignup, error)
+	Update(obj *crtapi.UserSignup) (*crtapi.UserSignup, error)
+	Delete(name string, options *v1.DeleteOptions) error
+}
+
+type userSignupClientImpl struct {
+	client rest.Interface
+	ns     string
+}
+
+func (c *userSignupClientImpl) Get(name string) (*crtapi.UserSignup, error) {
 	result := &crtapi.UserSignup{}
-
-	err := c.Client.
-		Get().
-		Resource("UserSignup").
+	err := c.client.Get().
+		Namespace(c.ns).
+		Resource(USER_SIGNUP_RESOURCE_NAME).
+		Name(name).
 		Do().
 		Into(result)
+	return result, err
+}
 
-	if err != nil {
-		return nil, err
-	}
+func (c *userSignupClientImpl) Create(obj *crtapi.UserSignup) (*crtapi.UserSignup, error) {
+	result := &crtapi.UserSignup{}
+	err := c.client.Post().
+		Namespace(c.ns).
+		Resource(USER_SIGNUP_RESOURCE_NAME).
+		Body(obj).
+		Do().
+		Into(result)
+	return result, err
+}
 
-	return result, nil
+func (c *userSignupClientImpl) Update(obj *crtapi.UserSignup) (*crtapi.UserSignup, error) {
+	result := &crtapi.UserSignup{}
+	err := c.client.Put().
+		Namespace(c.ns).
+		Resource(USER_SIGNUP_RESOURCE_NAME).
+		Body(obj).
+		Do().
+		Into(result)
+	return result, err
+}
+
+func (c *userSignupClientImpl) Delete(name string, options *v1.DeleteOptions) error {
+	return c.client.Delete().
+		Namespace(c.ns).
+		Resource(USER_SIGNUP_RESOURCE_NAME).
+		Name(name).
+		Body(options).
+		Do().
+		Error()
 }
--- pkg/signup/signup_service.go
+++ pkg/signup/signup_service.go
@@ -2,12 +2,85 @@ package signup
 
 import (
 	"context"
+	"fmt"
+	crtapi "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"strings"
 )
 
 type SignupService interface {
-	CreateUserSignup(ctx context.Context, username, userID string) error
+	CreateUserSignup(ctx context.Context, username, userID string) (*crtapi.UserSignup, error)
 }
 
-type signupClientImpl struct {
-	Clientset apiextension.Interface
+type signupServiceImpl struct {
+	client UserSignupClient
+}
+
+func NewSignupService(namespace string) (SignupService, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := NewUserSignupClient(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &signupServiceImpl{
+		client: client.UserSignups(namespace),
+	}, nil
+}
+
+func (c *signupServiceImpl) CreateUserSignup(ctx context.Context, username, userID string) (*crtapi.UserSignup, error) {
+	name, err := c.transformAndValidateUserName(username)
+	if err != nil {
+		return nil, err
+	}
+
+	userSignup := &crtapi.UserSignup{
+		ObjectMeta: v1.ObjectMeta{
+			Name: name,
+		},
+		Spec: crtapi.UserSignupSpec{
+			UserID:        userID,
+			TargetCluster: "",
+			Approved:      false,
+			Username:      username,
+		},
+	}
+
+	created, err := c.client.Create(userSignup)
+	if err != nil {
+		return nil, err
+	}
+
+	return created, nil
+}
+
+func (c *signupServiceImpl) transformAndValidateUserName(username string) (string, error) {
+	replaced := strings.ReplaceAll(strings.ReplaceAll(username, "@", "-at-"), ".", "-")
+
+	iteration := 0
+	transformed := replaced
+
+	for {
+		userSignup, err := c.client.Get(transformed)
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				return "", err
+			}
+		}
+
+		if userSignup == nil {
+			break
+		}
+
+		iteration++
+		transformed = fmt.Sprintf("%s-%d", replaced, iteration)
+	}
+
+	return transformed, nil
 }

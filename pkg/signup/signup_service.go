1371b23ace6b80fcafa5ff1389290ae1f84519e4
Shane Bryzak <sbryzak@gmail.com>
2019-09-19 20:40:11 +1000

    CRT-245 lots of refactoring underway...
    Signed-off-by: Shane Bryzak <sbryzak@gmail.com>

--- pkg/signup/signup_service.go
+++ pkg/signup/signup_service.go
@@ -2,92 +2,12 @@ package signup
 
 import (
 	"context"
-	"fmt"
-	crtapi "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
-	"strings"
 )
 
-type SignupServiceConfiguration interface {
-	GetNamespace() string
-}
-
 type SignupService interface {
-	CreateUserSignup(ctx context.Context, username, userID string) (*crtapi.UserSignup, error)
+	CreateUserSignup(ctx context.Context, username, userID string) error
 }
 
-type SignupServiceImpl struct {
-	Namespace string
-	Client    UserSignupClient
-}
-
-func NewSignupService(cfg SignupServiceConfiguration) (SignupService, error) {
-	k8sConfig, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := NewUserSignupClient(k8sConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return &SignupServiceImpl{
-		Namespace: cfg.GetNamespace(),
-		Client:    client.UserSignups(cfg.GetNamespace()),
-	}, nil
-}
-
-func (c *SignupServiceImpl) CreateUserSignup(ctx context.Context, username, userID string) (*crtapi.UserSignup, error) {
-	name, err := c.transformAndValidateUserName(username)
-	if err != nil {
-		return nil, err
-	}
-
-	userSignup := &crtapi.UserSignup{
-		ObjectMeta: v1.ObjectMeta{
-			Name:      name,
-			Namespace: c.Namespace,
-		},
-		Spec: crtapi.UserSignupSpec{
-			UserID:        userID,
-			TargetCluster: "",
-			Approved:      false,
-			Username:      username,
-		},
-	}
-
-	created, err := c.Client.Create(userSignup)
-	if err != nil {
-		return nil, err
-	}
-
-	return created, nil
-}
-
-func (c *SignupServiceImpl) transformAndValidateUserName(username string) (string, error) {
-	replaced := strings.ReplaceAll(strings.ReplaceAll(username, "@", "-at-"), ".", "-")
-
-	iteration := 0
-	transformed := replaced
-
-	for {
-		userSignup, err := c.Client.Get(transformed)
-		if err != nil {
-			if !errors.IsNotFound(err) {
-				return "", err
-			}
-		}
-
-		if userSignup == nil {
-			break
-		}
-
-		iteration++
-		transformed = fmt.Sprintf("%s-%d", replaced, iteration)
-	}
-
-	return transformed, nil
+type signupClientImpl struct {
+	Clientset apiextension.Interface
 }

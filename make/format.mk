6ffc05d7613d65fbe4112704ec5da370a91e7023
tinakurian <tkurian@redhat.com>
2019-09-24 13:56:44 -0400

    formatting

--- make/format.mk
+++ make/format.mk
@@ -2,3 +2,12 @@
 ## format go code
 format:
 	gofmt -s -l -w $(shell find  . -name '*.go' | grep -vEf .gofmt_exclude)
+
+check-go-format:
+	@gofmt -s -l $(shell find  . -name '*.go' | grep -vEf .gofmt_exclude) 2>&1 \
+		| tee /tmp/gofmt-errors \
+		| read \
+	&& echo "ERROR: These files differ from gofmt's style (run 'make format-go-code' to fix this):" \
+	&& cat /tmp/gofmt-errors \
+	&& exit 1 \
+	|| true

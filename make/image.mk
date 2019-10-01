0a063af1b8d3834499d0fe8710adcdf37d616973
Alexey Kazakov <alkazako@redhat.com>
2019-09-30 21:39:07 -0700

    Readme

--- make/image.mk
+++ make/image.mk
@@ -1,7 +1,9 @@
-IMAGE_NAME?=quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:latest
+TIMESTAMP:=$(shell date +%s)
+TAG?=$(GIT_COMMIT_ID_SHORT)-$(TIMESTAMP)
+IMAGE_NAME?=quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${TAG}
 
 .PHONY: image
 ## Build the docker image locally that can be deployed (only contains bare registration-service)
 image: build
-	$(Q)docker build -f build/Dockerfile -t quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${GIT_COMMIT_ID_SHORT} \
+	$(Q)docker build -f build/Dockerfile -t quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:latest \
 	 -t ${IMAGE_NAME} .

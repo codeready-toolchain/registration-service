5893a7dcf0a107b8f5ee3c2257b0e8b679c2b8dc
Alexey Kazakov <alkazako@redhat.com>
2019-10-02 18:28:08 -0700

    Move image push to quay

--- docker_push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push kleinhenz/registration-service:0.1
-docker push kleinhenz/registration-service:latest

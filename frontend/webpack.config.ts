e2bb6ebfd9fadac1712296f00d703ee41790bed5
Abhishek K N <akyathan@redhat.com>
2019-11-14 16:32:53 +0530

    Adding frontend infra setup

---
+++ frontend/webpack.config.ts
@@ -0,0 +1,156 @@
+/* eslint-env node */
+import * as webpack from 'webpack';
+import * as path from 'path';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import * as ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+interface Configuration extends webpack.Configuration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const NODE_ENV = process.env.NODE_ENV;
+const HOT_RELOAD = process.env.HOT_RELOAD;
+
+/* Helpers */
+const extractCSS = new MiniCssExtractPlugin({ filename: 'app-bundle.css' });
+const overpassTest = /overpass-.*\.(woff2?|ttf|eot|otf)(\?.*$|$)/;
+
+const config: Configuration = {
+  entry: [
+    './polyfills.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: 'static/',
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-[chunkhash].js',
+  },
+  devServer: {
+    writeToDisk: true,
+    progress: true,
+    hot: HOT_RELOAD !== 'false',
+    inline: HOT_RELOAD !== 'false',
+    contentBase: path.join(__dirname, "public/dist"),
+    port: 9000
+  },
+  resolve: {
+    extensions: ['.glsl', '.ts', '.tsx', '.js', '.jsx'],
+  },
+  node: {
+    fs: 'empty',
+    // eslint-disable-next-line camelcase
+    child_process: 'empty',
+    net: 'empty',
+    crypto: 'empty',
+    module: 'empty',
+  },
+  module: {
+    rules: [
+      { test: /\.glsl$/, loader: 'raw!glslify' },
+      {
+        test: /(\.jsx?)|(\.tsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'cache-loader' },
+          {
+            loader: 'thread-loader',
+            options: {
+              // Leave one core spare for fork-ts-checker-webpack-plugin
+              workers: require('os').cpus().length - 1,
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true, // This implicitly enables transpileOnly! No type checking!
+              transpileOnly: true, // fork-ts-checker-webpack-plugin takes care of type checking
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules\/(?!(@patternfly)\/).*/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          { loader: 'cache-loader' },
+          { loader: 'thread-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              outputStyle: 'compressed',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
+        exclude: overpassTest,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]',
+        },
+      },
+      {
+        // Resolve to an empty module for overpass fonts included in SASS files.
+        // This way file-loader won't parse them. Make sure this is BELOW the
+        // file-loader rule.
+        test: overpassTest,
+        loader: 'null-loader',
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: true,
+  },
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(/^lodash$/, 'lodash-es'),
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }),
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './public/index.html',
+      production: NODE_ENV === 'production',
+      chunksSortMode: 'none',
+    }),
+    new webpack.IgnorePlugin(/prettier/),
+    extractCSS,
+  ],
+  devtool: 'cheap-module-source-map',
+  stats: 'minimal',
+};
+
+/* Production settings */
+if (NODE_ENV === 'production') {
+  config.output.filename = '[name]-bundle-[hash].min.js';
+  config.output.chunkFilename = '[name]-chunk-[chunkhash].min.js';
+  extractCSS.filename = '[name]-[chunkhash].min.css';
+  // Causes error in --mode=production due to scope hoisting
+  config.optimization.concatenateModules = false;
+  config.stats = 'normal';
+}
+
+export default config;

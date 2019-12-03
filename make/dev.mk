cd179fbfc320bcca6f6adba497ebf7a8c4957e6e
Matous Jobanek <matousjobanek@gmail.com>
2019-12-03 09:01:15 +0100

    chore(CRT-361):  move SA, RBAC and namespace def into deployment template (#75)

--- make/dev.mk
+++ make/dev.mk
@@ -37,21 +37,15 @@ clean-namespace:
 
 .PHONY: reset-namespace
 ## Delete an create the test namespace and deploy rbac there
-reset-namespace: login-as-admin clean-namespace create-namespace deploy-rbac
-
-.PHONY: deploy-rbac
-## Setup service account and deploy RBAC
-deploy-rbac:
-	$(Q)oc apply -f deploy/service_account.yaml
-	$(Q)oc apply -f deploy/role.yaml
-	$(Q)oc apply -f deploy/role_binding.yaml
+reset-namespace: login-as-admin clean-namespace create-namespace
 
 .PHONY: deploy-dev
 ## Deploy Registration service on minishift
-deploy-dev: login-as-admin create-namespace deploy-rbac build dev-image
-	$(Q)oc process -f ./deploy/deployment.yaml \
+deploy-dev: login-as-admin create-namespace build dev-image
+	$(Q)oc process -f ./deploy/registration-service.yaml \
         -p IMAGE=${IMAGE_NAME_DEV} \
         -p ENVIRONMENT=dev \
+        -p NAMESPACE=${LOCAL_TEST_NAMESPACE} \
         | oc apply -f -
 
 .PHONY: deploy-e2e

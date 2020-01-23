fd0dad027582fe2734b0705353d072aa2f12f29a
Matous Jobanek <mjobanek@redhat.com>
2020-01-23 15:04:15 +0000

    add docker targets

--- make/dev.mk
+++ make/dev.mk
@@ -2,8 +2,6 @@ MINISHIFT_IP?=$(shell minishift ip)
 MINISHIFT_HOSTNAME=minishift.local
 MINISHIFT_HOSTNAME_REGEX='minishift\.local'
 ETC_HOSTS=/etc/hosts
-TIMESTAMP:=$(shell date +%s)
-IMAGE_NAME_DEV?=${IMAGE_NAME}-$(TIMESTAMP)
 
 # to watch all namespaces, keep namespace empty
 APP_NAMESPACE ?= $(LOCAL_TEST_NAMESPACE)
@@ -41,9 +39,9 @@ reset-namespace: login-as-admin clean-namespace create-namespace
 
 .PHONY: deploy-dev
 ## Deploy Registration service on minishift
-deploy-dev: login-as-admin create-namespace build dev-image
+deploy-dev: login-as-admin create-namespace build docker-image-dev
 	$(Q)oc process -f ./deploy/registration-service.yaml \
-        -p IMAGE=${IMAGE_NAME_DEV} \
+        -p IMAGE=${IMAGE_DEV} \
         -p ENVIRONMENT=dev \
         -p NAMESPACE=${LOCAL_TEST_NAMESPACE} \
         | oc apply -f -
@@ -52,9 +50,3 @@ deploy-dev: login-as-admin create-namespace build dev-image
 deploy-e2e: get-e2e-repo
 ## Deploy the e2e resources with the local 'registration-service' repository only
 	$(MAKE) -C ${E2E_REPO_PATH} dev-deploy-e2e REG_REPO_PATH=${PWD}
-
-.PHONY: dev-image
-## Build the docker image locally that can be deployed to dev environment
-dev-image: build
-	$(Q)docker build -f build/Dockerfile -t quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:latest \
-	 -t ${IMAGE_NAME_DEV} .

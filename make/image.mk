dbcd4e30f262bf0568c5dfeb41d659b839558566
Matous Jobanek <mjobanek@redhat.com>
2020-01-24 18:01:53 +0000

    chore(CRT-362): add travis job generating new version of host-operator CSV (#89)

--- make/image.mk
+++
@@ -1,8 +0,0 @@
-TAG?=$(GIT_COMMIT_ID_SHORT)
-IMAGE_NAME?=quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${TAG}
-
-.PHONY: image
-## Build the docker image locally that can be deployed (only contains bare registration-service)
-image: build
-	$(Q)docker build -f build/Dockerfile -t quay.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:latest \
-	 -t ${IMAGE_NAME} .

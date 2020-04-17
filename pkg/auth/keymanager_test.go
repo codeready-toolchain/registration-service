20b8f43d0f0e2dbbbba8a5191c8700ede9c98107
tinakurian <tkurian@redhat.com>
2020-04-16 21:54:43 -0400

    updating test

--- pkg/auth/keymanager_test.go
+++ pkg/auth/keymanager_test.go
@@ -145,7 +145,7 @@ func (s *TestKeyManagerSuite) TestKeyFetching() {
 		// Create KeyManager instance.
 		_, err := auth.NewKeyManager(s.Config)
 		// this needs to fail with an error
-		assert.EqualError(s.T(), err, "Get \"not%20an%20url\": unsupported protocol scheme \"\"")
+		assert.EqualError(s.T(), err, "Get not%20an%20url: unsupported protocol scheme \"\"")
 	})
 
 	s.Run("parse keys, server not reachable", func() {

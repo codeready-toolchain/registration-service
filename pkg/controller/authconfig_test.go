9cd7389e2bce58fc761ef31edafe78c4df0c0ded
tinakurian <tkurian@redhat.com>
2020-08-17 12:22:26 -0400

    fixing tests

--- pkg/controller/authconfig_test.go
+++ pkg/controller/authconfig_test.go
@@ -1,7 +1,14 @@
 package controller
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/codeready-toolchain/registration-service/test"
 
@@ -16,96 +23,75 @@ func TestRunAuthClientConfigSuite(t *testing.T) {
 	suite.Run(t, &TestAuthConfigSuite{test.UnitTestSuite{}})
 }
 
-//func (s *TestAuthConfigSuite) TestAuthClientConfigHandler() {
-//	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-//	// pass 'nil' as the third parameter.
-//	req, err := http.NewRequest(http.MethodGet, "/api/v1/authconfig", nil)
-//	require.NoError(s.T(), err)
-//
-//	// Check if the config is set to testing mode, so the handler may use this.
-//	assert.True(s.T(), s.Config.IsTestingMode(), "testing mode not set correctly to true")
-//
-//	// Create handler instance.
-//	authConfigCtrl := NewAuthConfig(s.Config)
-//	handler := gin.HandlerFunc(authConfigCtrl.GetHandler)
-//
-//	s.Run("valid json config", func() {
-//
-//		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-//		rr := httptest.NewRecorder()
-//		ctx, _ := gin.CreateTestContext(rr)
-//		ctx.Request = req
-//
-//		handler(ctx)
-//
-//		// Check the status code is what we expect.
-//		require.Equal(s.T(), http.StatusOK, rr.Code)
-//
-//		// check response content-type.
-//		require.Equal(s.T(), s.Config.GetAuthClientConfigAuthContentType(), rr.Header().Get("Content-Type"))
-//
-//		// Check the response body is what we expect.
-//		// get config values from endpoint response
-//		var dataEnvelope *configResponse
-//		err = json.Unmarshal(rr.Body.Bytes(), &dataEnvelope)
-//		require.NoError(s.T(), err)
-//
-//		// get the configured values
-//		var config map[string]interface{}
-//		err = json.Unmarshal([]byte(s.Config.GetAuthClientConfigAuthRaw()), &config)
-//		require.NoError(s.T(), err)
-//
-//		s.Run("envelope client url", func() {
-//			assert.Equal(s.T(), s.Config.GetAuthClientLibraryURL(), dataEnvelope.AuthClientLibraryURL, "wrong 'auth-client-library-url' in authconfig response")
-//		})
-//
-//		s.Run("envelope client config", func() {
-//			assert.NotEmpty(s.T(), dataEnvelope.AuthClientLibraryURL, "no 'auth-client-config' key in authconfig response")
-//
-//			// deserialize the values in the string field
-//			var data map[string]interface{}
-//			err = json.Unmarshal([]byte(dataEnvelope.AuthClientConfigRaw), &data)
-//			require.NoError(s.T(), err)
-//
-//			s.Run("realm", func() {
-//				val, ok := data["realm"]
-//				assert.True(s.T(), ok, "no 'realm' key in authconfig response")
-//				valString, ok := val.(string)
-//				assert.True(s.T(), ok, "returned 'realm' value is not of type 'string'")
-//				assert.Equal(s.T(), config["realm"], valString, "wrong 'realm' in authconfig response")
-//			})
-//
-//			s.Run("auth-server-url", func() {
-//				val, ok := data["auth-server-url"]
-//				assert.True(s.T(), ok, "no 'auth-server-url' key in authconfig response")
-//				valString, ok := val.(string)
-//				assert.True(s.T(), ok, "returned 'auth-server-url' value is not of type 'string'")
-//				assert.Equal(s.T(), config["auth-server-url"], valString, "wrong 'auth-server-url' in authconfig response")
-//			})
-//
-//			s.Run("ssl-required", func() {
-//				val, ok := data["ssl-required"]
-//				assert.True(s.T(), ok, "no 'ssl-required' key in authconfig response")
-//				valString, ok := val.(string)
-//				assert.True(s.T(), ok, "returned 'ssl-required' value is not of type 'string'")
-//				assert.Equal(s.T(), config["ssl-required"], valString, "wrong 'ssl-required' in authconfig response")
-//			})
-//
-//			s.Run("resource", func() {
-//				val, ok := data["resource"]
-//				assert.True(s.T(), ok, "no 'resource' key in authconfig response")
-//				valString, ok := val.(string)
-//				assert.True(s.T(), ok, "returned 'resource' value is not of type 'string'")
-//				assert.Equal(s.T(), config["resource"], valString, "wrong 'resource' in authconfig response")
-//			})
-//
-//			s.Run("public-client", func() {
-//				val, ok := data["public-client"]
-//				assert.True(s.T(), ok, "no 'public-client' key in authconfig response")
-//				valBool, ok := val.(bool)
-//				assert.True(s.T(), ok, "returned 'public-client' value is not of type 'bool'")
-//				assert.Equal(s.T(), config["public-client"], valBool, "wrong 'public-client' in authconfig response")
-//			})
-//		})
-//	})
-//}
+func (s *TestAuthConfigSuite) TestAuthClientConfigHandler() {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest(http.MethodGet, "/api/v1/authconfig", nil)
+	require.NoError(s.T(), err)
+
+	// Check if the config is set to testing mode, so the handler may use this.
+	assert.True(s.T(), s.Config.IsTestingMode(), "testing mode not set correctly to true")
+
+	// Create handler instance.
+	authConfigCtrl := NewAuthConfig(s.Config)
+	handler := gin.HandlerFunc(authConfigCtrl.GetHandler)
+
+	s.Run("valid json config", func() {
+
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
+		ctx, _ := gin.CreateTestContext(rr)
+		ctx.Request = req
+
+		handler(ctx)
+
+		// Check the status code is what we expect.
+		require.Equal(s.T(), http.StatusOK, rr.Code)
+
+		// check response content-type.
+		require.Equal(s.T(), s.Config.GetAuthClientConfigAuthContentType(), rr.Header().Get("Content-Type"))
+
+		// Check the response body is what we expect.
+		// get config values from endpoint response
+		var dataEnvelope *configResponse
+		err = json.Unmarshal(rr.Body.Bytes(), &dataEnvelope)
+		require.NoError(s.T(), err)
+
+		s.Run("envelope client url", func() {
+			assert.Equal(s.T(), s.Config.GetAuthClientLibraryURL(), dataEnvelope.AuthClientLibraryURL, "wrong 'auth-client-library-url' in authconfig response")
+		})
+
+		s.Run("envelope client config", func() {
+			assert.NotEmpty(s.T(), dataEnvelope.AuthClientLibraryURL, "no 'auth-client-config' key in authconfig response")
+
+			s.Run("realm", func() {
+				assert.Equal(s.T(), s.Config.GetAuthClientConfigRawRealm(), dataEnvelope.AuthClientConfigRawRealm, "wrong 'realm' in authconfig response")
+			})
+
+			s.Run("auth-server-url", func() {
+				assert.Equal(s.T(), s.Config.GetAuthClientConfigRawAuthServerURL(), dataEnvelope.AuthClientConfigRawServerURL, "wrong 'auth-server-url' in authconfig response")
+			})
+
+			s.Run("ssl-required", func() {
+				assert.Equal(s.T(), s.Config.GetAuthClientConfigRawSSLRequired(), dataEnvelope.AuthClientConfigRawSSLRequired, "wrong 'ssl-required' in authconfig response")
+			})
+
+			s.Run("resource", func() {
+				assert.Equal(s.T(), s.Config.GetAuthClientConfigRawResource(), dataEnvelope.AuthClientConfigRawResource, "wrong 'resource' in authconfig response")
+			})
+
+			s.Run("public-client", func() {
+				assert.Equal(s.T(), s.Config.GetAuthClientConfigRawPublicClient(), dataEnvelope.AuthClientConfigRawPublicClient, "wrong 'public-client' in authconfig response")
+			})
+			s.Run("verification-daily-limit", func() {
+				assert.Equal(s.T(), s.Config.GetVerificationDailyLimit(), dataEnvelope.VerificationDailyLimit, "wrong 'verification daily limit' in authconfig response")
+			})
+			s.Run("verification-attempts-allowed", func() {
+				assert.Equal(s.T(), s.Config.GetVerificationAttemptsAllowed(), dataEnvelope.VerificationAttemptsAllowed, "wrong 'verification attempts allowed' in authconfig response")
+			})
+			s.Run("verification-enabled", func() {
+				assert.Equal(s.T(), s.Config.GetVerificationEnabled(), dataEnvelope.VerificationEnabled, "wrong 'verification enabled' in authconfig response")
+			})
+		})
+	})
+}

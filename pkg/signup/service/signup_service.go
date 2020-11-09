6a0ebe94fa7132ba7d004b13e3bdc133a3579991
Xavier Coulon <xcoulon@redhat.com>
2020-11-09 10:10:27 +0100

    update api and common modules (#139)
    also, remove outdated labels on new usersignups
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/signup/service/signup_service.go
+++ pkg/signup/service/signup_service.go
@@ -90,7 +90,6 @@ func (s *ServiceImpl) CreateUserSignup(ctx *gin.Context) (*v1alpha1.UserSignup, 
 			},
 			Labels: map[string]string{
 				v1alpha1.UserSignupUserEmailHashLabelKey: emailHash,
-				v1alpha1.UserSignupApprovedLabelKey:      v1alpha1.UserSignupApprovedLabelValueFalse,
 			},
 		},
 		Spec: v1alpha1.UserSignupSpec{

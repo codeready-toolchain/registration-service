5cc1588595e2fc5e68c5173d46d3c495d55db363
Tina Kurian <tinakurian@localhost.localdomain>
2020-11-18 13:30:06 -0500

    fixing test

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -36,42 +36,27 @@ func (s *TestServerSuite) TestServer() {
 
 	// Check that Engine() returns the router object.
 	require.NotNil(s.T(), srv.Engine())
-}
 
-func (s *TestServerSuite) TestCrossCors() {
-	// We're using the example config for the configuration here as the
-	// specific config params do not matter for testing the routes setup.
-	srv := server.New(s.Config(), fake.NewMockableApplication(s.Config(), nil))
+	s.T().Run("CORS", func(t *testing.T) {
+		go srv.Engine().Run()
 
-	// Setting up the routes.
-	err := srv.SetupRoutes()
-	require.NoError(s.T(), err)
+		req, err := http.NewRequest("OPTIONS", "http://localhost:8080/api/v1/authconfig", nil)
+		require.NoError(s.T(), err)
 
-	// Check that there are routes registered.
-	routes := srv.GetRegisteredRoutes()
-	require.NotEmpty(s.T(), routes)
+		req.Header.Set("Origin", "http://example.com")
+		client := &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+		}
+		resp, err := client.Do(req)
+		require.NoError(s.T(), err)
+		defer resp.Body.Close()
 
-	// Check that Engine() returns the router object.
-	require.NotNil(s.T(), srv.Engine())
-
-	go srv.Engine().Run()
-
-	req, err := http.NewRequest("OPTIONS", "http://localhost:8080/api/v1/authconfig", nil)
-	require.NoError(s.T(), err)
-
-	req.Header.Set("Origin", "http://example.com")
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
-	resp, err := client.Do(req)
-	require.NoError(s.T(), err)
-	defer resp.Body.Close()
-
-	require.Equal(s.T(), 204, resp.StatusCode)
-	require.Equal(s.T(), "Content-Length,Content-Type,Authorization,Accept", resp.Header.Get("Access-Control-Allow-Headers"))
-	require.Equal(s.T(), "PUT,PATCH,POST,GET,DELETE,OPTIONS", resp.Header.Get("Access-Control-Allow-Methods"))
-	require.Equal(s.T(), "*", resp.Header.Get("Access-Control-Allow-Origin"))
-	require.Equal(s.T(), "true", resp.Header.Get("Access-Control-Allow-Credentials"))
+		require.Equal(s.T(), 204, resp.StatusCode)
+		require.Equal(s.T(), "Content-Length,Content-Type,Authorization,Accept", resp.Header.Get("Access-Control-Allow-Headers"))
+		require.Equal(s.T(), "PUT,PATCH,POST,GET,DELETE,OPTIONS", resp.Header.Get("Access-Control-Allow-Methods"))
+		require.Equal(s.T(), "*", resp.Header.Get("Access-Control-Allow-Origin"))
+		require.Equal(s.T(), "true", resp.Header.Get("Access-Control-Allow-Credentials"))
+	})
 }

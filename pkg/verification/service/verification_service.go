f1be526c142608bc1ee8a232c4c96f27f00d01e0
Tina Kurian <tinakurian@localhost.localdomain>
2021-03-01 11:17:58 -0500

    fixing errors

--- pkg/verification/service/verification_service.go
+++ pkg/verification/service/verification_service.go
@@ -206,8 +206,7 @@ func (s *ServiceImpl) VerifyCode(ctx *gin.Context, userID string, code string) e
 	err := s.Services().SignupService().PhoneNumberAlreadyInUse(userID, signup.Labels[v1alpha1.UserSignupUserPhoneHashLabelKey])
 	if err != nil {
 		log.Error(ctx, err, "phone number to verify already in use")
-		return errors.NewBadRequest("phone number already in use",
-			"the phone number provided for this signup is already in use by an active account")
+		return errors.NewBadRequest("phone number already in use:the phone number provided for this signup is already in use by an active account")
 	}
 
 	now := time.Now()

0389f545447adfe82a3d42057805a7c553b3182d
Alexey Kazakov <alkazako@redhat.com>
2021-11-15 18:22:49 -0800

    lint

--- pkg/proxy/namespace/namespace.go
+++ pkg/proxy/namespace/namespace.go
@@ -3,7 +3,7 @@ package namespace
 import "net/url"
 
 // NamespaceAccess holds an information needed to access the namespace in a member cluster for the specific user
-type NamespaceAccess struct {
+type NamespaceAccess struct { // nolint: golint
 	// APIURL is the Cluster API Endpoint for the namespace
 	APIURL url.URL
 	// SAToken is a token of the Service Account which represents the user in the namespace

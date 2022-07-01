4921fa04d7478ca3db740f60fcd9f2403400d9a2
Xavier Coulon <xcoulon@redhat.com>
2022-07-01 11:52:51 +0200

    test: scripts to build/deploy on OpenShift
    2 scripts:
    - a script to expose the Container Registry with a
      public route
    - a script to build/push the Docker image, then patch
      the deployment
    
    also: changed the Dockerfile to fasten the builds
    (fewer layers to change after the binary was copied)
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ scripts/setup-registry.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+#------------------------------------------------------------------
+# Configure a public route to access the Container Registry 
+# of the OpenShift cluster.
+# (required before build/pushing new images with `redeploy-app.sh`)
+#------------------------------------------------------------------
+echo "🛠 exposing the registry using the default route"
+oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
+REGISTRY_ROUTE=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+echo "registry route: https://$REGISTRY_ROUTE"
+
+# scale down the deployment to 1 replica (a single pod is enough when working on the UI)
+echo "⚙️ scaling down the registration-service"
+oc scale --replicas=1 deployment/registration-service

8df3d72a5ccbf4bff7a7838c6b8dd40bf3f032ec
Rajiv Senthilnathan <rajivnathan@gmail.com>
2023-01-16 22:20:42 -0500

    Fix lint error

--- test/fake/informer.go
+++ test/fake/informer.go
@@ -4,31 +4,31 @@ import (
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 )
 
-func NewFakeInformer() *FakeInformer {
-	return &FakeInformer{}
+func NewFakeInformer() *Informer {
+	return &Informer{}
 }
 
-type FakeInformer struct {
+type Informer struct {
 	GetMurFunc             func(name string) (*toolchainv1alpha1.MasterUserRecord, error)
 	GetToolchainStatusFunc func() (*toolchainv1alpha1.ToolchainStatus, error)
 	GetUserSignupFunc      func(name string) (*toolchainv1alpha1.UserSignup, error)
 }
 
-func (f FakeInformer) GetMasterUserRecord(name string) (*toolchainv1alpha1.MasterUserRecord, error) {
+func (f Informer) GetMasterUserRecord(name string) (*toolchainv1alpha1.MasterUserRecord, error) {
 	if f.GetMurFunc != nil {
 		return f.GetMurFunc(name)
 	}
 	panic("not supposed to call GetMasterUserRecord")
 }
 
-func (f FakeInformer) GetToolchainStatus() (*toolchainv1alpha1.ToolchainStatus, error) {
+func (f Informer) GetToolchainStatus() (*toolchainv1alpha1.ToolchainStatus, error) {
 	if f.GetToolchainStatusFunc != nil {
 		return f.GetToolchainStatusFunc()
 	}
 	panic("not supposed to call GetToolchainStatus")
 }
 
-func (f FakeInformer) GetUserSignup(name string) (*toolchainv1alpha1.UserSignup, error) {
+func (f Informer) GetUserSignup(name string) (*toolchainv1alpha1.UserSignup, error) {
 	if f.GetUserSignupFunc != nil {
 		return f.GetUserSignupFunc(name)
 	}

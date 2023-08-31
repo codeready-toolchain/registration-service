1d1a3ad6e42105ba01cbca410ca2fbc1420f8500
Shane Bryzak <sbryzak@gmail.com>
2023-09-01 07:10:26 +1000

    added another test

--- pkg/verification/sender/twilio_sender_test.go
+++ pkg/verification/sender/twilio_sender_test.go
@@ -37,7 +37,7 @@ func (c *MockTwilioConfig) TwilioSenderConfigs() []toolchainv1alpha1.TwilioSende
 	return c.SenderConfigs
 }
 
-func TestTwilioSenderIDs(t *testing.T) {
+func TestTwilioSenderIDConfigFound(t *testing.T) {
 
 	httpClient := &http.Client{Transport: &http.Transport{}}
 	gock.InterceptClient(httpClient)
@@ -89,5 +89,58 @@ func TestTwilioSenderIDs(t *testing.T) {
 	require.Equal(t, "Test Message", v.Get("Body"))
 	require.Equal(t, "RED HAT", v.Get("From"))
 	require.Equal(t, "+440000000000", v.Get("To"))
+}
 
+func TestTwilioSenderIDCountryNotConfigured(t *testing.T) {
+
+	httpClient := &http.Client{Transport: &http.Transport{}}
+	gock.InterceptClient(httpClient)
+
+	defer gock.Off()
+
+	gock.New("https://api.twilio.com").
+		Reply(http.StatusNoContent).
+		BodyString("")
+
+	var reqBody io.ReadCloser
+	obs := func(request *http.Request, mock gock.Mock) {
+		reqBody = request.Body
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			require.NoError(t, err)
+		}(request.Body)
+	}
+
+	gock.Observe(obs)
+
+	ctx, _ := gin.CreateTestContext(httptest.NewRecorder())
+
+	cfg := &MockTwilioConfig{
+		AccountSID: "TWILIO_SID_VALUE",
+		AuthToken:  "AUTH_TOKEN_VALUE",
+		FromNumber: "+13334445555",
+		SenderConfigs: []toolchainv1alpha1.TwilioSenderConfig{
+			{
+				SenderID:     "RED HAT",
+				CountryCodes: []string{"44"},
+			},
+		},
+	}
+
+	sender := sender2.NewTwilioSender(cfg, httpClient)
+
+	err := sender.SendNotification(ctx, "Test Message", "+611234567890", "61")
+	require.NoError(t, err)
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(reqBody)
+	require.NoError(t, err)
+	reqValue := buf.String()
+
+	v, err := url.ParseQuery(reqValue)
+	require.NoError(t, err)
+
+	require.Equal(t, "Test Message", v.Get("Body"))
+	require.Equal(t, "+13334445555", v.Get("From"))
+	require.Equal(t, "+611234567890", v.Get("To"))
 }

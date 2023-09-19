39f970dc90a218bfd778fe68728e670951e7ab4f
Kanika Rana <krana@redhat.com>
2023-09-19 00:04:28 -0400

    disable compression

--- pkg/controller/metrics.go
+++ pkg/controller/metrics.go
@@ -13,7 +13,12 @@ func NewMetrics() *Metrics {
 }
 
 func (m *Metrics) PrometheusHandler(c *gin.Context) {
-	h := promhttp.HandlerFor(metrics.Reg, promhttp.HandlerOpts{Registry: metrics.Reg})
+	h := promhttp.HandlerFor(metrics.Reg, promhttp.HandlerOpts{DisableCompression: true, Registry: metrics.Reg})
+	c.Writer.Header().Del("content-encoding")
+	c.Writer.Header().Del("vary")
 	c.Writer.Header().Set("content-Type", "text/plain")
+	c.Request.Header.Set("content-Type", "text/plain")
+	c.Request.Header.Del("content-encoding")
+	c.Request.Header.Del("vary")
 	h.ServeHTTP(c.Writer, c.Request)
 }

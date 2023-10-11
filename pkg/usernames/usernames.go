4eae185a6e9748c47bde529bd511fc58c6e59f8a
Francisc Munteanu <fmuntean@redhat.com>
2023-10-11 12:57:56 +0200

    create usernames type

---
+++ pkg/usernames/usernames.go
@@ -0,0 +1,19 @@
+package usernames
+
+// Usernames represents the response of a search request done by using the MUR name or an email address.
+// The result will specify if a user exits in the system or not , or if there are multiple matches for the given search query
+type Usernames struct {
+	// For is the search string that was specified in the search request url. Example: /usernames/<search string>
+	For string `json:"for"`
+	// Valid can be true or false.
+	// 	true indicates that a match was found
+	// 	false indicates that no match was found
+	Valid string `json:"valid"`
+	// Matched is a list of the username matches found using the given search query.
+	// There might be more than one match, for example when using email id for the search request.
+	Matched []match `json:"matched,omitempty"`
+}
+
+type match struct {
+	Username string `json:"username"`
+}

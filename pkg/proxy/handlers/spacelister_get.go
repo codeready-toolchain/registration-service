c34dfad3d890e827b4e7cab8be9a67426c4e1fb4
Francesco Ilario <filario@redhat.com>
2024-08-01 11:11:42 +0200

    fix logs
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- pkg/proxy/handlers/spacelister_get.go
+++ pkg/proxy/handlers/spacelister_get.go
@@ -91,7 +91,7 @@ func getUserOrPublicViewerSpaceBinding(ctx echo.Context, spaceLister *SpaceListe
 				return nil, err
 			}
 			if pvSb == nil {
-				ctx.Logger().Errorf("unauthorized access - there is no SpaceBinding present for the user %s and the workspace %s", toolchainv1alpha1.KubesawAuthenticatedUsername, workspaceName)
+				ctx.Logger().Errorf("unauthorized access - there is no SpaceBinding present for the user %s or %s and the workspace %s", userSignup.CompliantUsername, toolchainv1alpha1.KubesawAuthenticatedUsername, workspaceName)
 				return nil, nil
 			}
 			return pvSb, nil

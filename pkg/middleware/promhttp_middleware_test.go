d097a29f668d7c5f061af62b300033dce5187b45
Xavier Coulon <xcoulon@redhat.com>
2025-03-27 16:26:06 +0100

    Update pkg/middleware/promhttp_middleware_test.go
    Co-authored-by: Lukas Krejci <metlos@users.noreply.github.com>

--- pkg/middleware/promhttp_middleware_test.go
+++ pkg/middleware/promhttp_middleware_test.go
@@ -100,7 +100,7 @@ func assertMetricExists(t *testing.T, data []byte, name string, labels map[strin
 			return
 		}
 	}
-	assert.Fail(t, "unable to find metric with labels", "name", name, "labels", labels)
+	assert.Fail(t, "unable to find metric with labels", "name: %s, labels: %v", name, labels)
 }
 
 func matchesLabels(x []*prommodel.LabelPair, y map[string]string) bool {

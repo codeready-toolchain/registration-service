0ad7cac6fb48cf91759ae556c7e9fe551868b25c
Xavier Coulon <xcoulon@redhat.com>
2026-03-24 17:52:41 +0100

    feat: expose metrics for the git commit
    similar to https://github.com/codeready-toolchain/host-operator/pull/1249
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -18,17 +18,6 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-var (
-	// Commit current build commit set by build script.
-	Commit = "0"
-	// BuildTime set by build script in ISO 8601 (UTC) format:
-	// YYYY-MM-DDThh:mm:ssTZD (see https://www.w3.org/TR/NOTE-datetime for
-	// details).
-	BuildTime = "0"
-	// StartTime in ISO 8601 (UTC) format.
-	StartTime = time.Now().UTC().Format("2006-01-02T15:04:05Z")
-)
-
 var logger = logf.Log.WithName("configuration")
 
 const (
